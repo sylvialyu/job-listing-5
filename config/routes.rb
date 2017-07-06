52c23f4fbf66a954070ace5b4609a595d1db9d3b
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-07-06 10:54:15 +0800

    implement admin#jobs CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root "jobs#index"
+  root "jobs#index"
 
-resources :jobs
+  resources :jobs
+
+  namespace :admin do
+    resources :jobs
+  end
 
 end
